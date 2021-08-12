131f461c79b0f4a3b14bb39d0170f2f6ec5c5cea
root <kis89576@naver.com>
2021-08-12 05:44:25 +0000

    2021/08/12

--- day4/ex03_rest_server/index.js
+++ day4/ex03_rest_server/index.js
@@ -1,5 +1,6 @@
 import http from 'http'
 import {URL} from 'url'
+import * as url from "url";
 
 const port = 8125
 
@@ -8,6 +9,9 @@ http.createServer((req,res) => {
         req.url,
         'http://a.com'
     );
+    console.log(`pathname : ${urlObj.pathname}`);
+    console.log(`search : ${urlObj.search}`);
+    console.log(`hostname : ${urlObj.hostname}`);
     let result = {
         r: 'ok'
     }
@@ -19,6 +23,24 @@ http.createServer((req,res) => {
         "Access-Control-Allow-Headers": "*" //CORS 정책 허용  * 는 모두 허용
     }
     res.writeHead(200, header);
+
+    if(urlObj.pathname === '/echo'){ //echo 요청 처리
+        let _title = urlObj.searchParams.get('title');
+        let _msg = urlObj.searchParams.get('msg');
+
+        result.title = _title;
+        result.msg = _msg;
+    }
+    else if(urlObj.pathname === '/sum'){ //add요청 처리
+        let _a = parseInt(urlObj.searchParams.get('a'));
+        let _b = parseInt(urlObj.searchParams.get('b'));
+        result.cal = _a + _b;
+    }
+    else if(urlObj.pathname === '/sub'){ //substract
+        let _a = parseInt(urlObj.searchParams.get('a'));
+        let _b = parseInt(urlObj.searchParams.get('b'));
+        result.cal = _a - _b;
+    }
     res.end(JSON.stringify(result));
 }).listen(port);
 
